f80c8ff562fd5aa635978c508b681083db2f6baf
phpdvlpmt <37871492+phpdvlpmt@users.noreply.github.com>
2023-12-31 19:55:55 +0100

    edit

--- src/app/quiz/[name]/page.jsx
+++ src/app/quiz/[name]/page.jsx
@@ -7,7 +7,7 @@ import { promises as fs } from 'fs';
 
 const Q = async ({params}) => {
  
-  const file = await fs.readFile(process.cwd() + '/' + params.name + '.json', 'utf8');
+  const file = await fs.readFile(process.cwd() + '/public/' + params.name + '.json', 'utf8');
   const data = JSON.parse(file);
     
   //const {quiz2 }= await import ("../../../../data2")
