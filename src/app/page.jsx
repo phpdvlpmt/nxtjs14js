4e1abfac7f1c83c4b9e130644711968e972388ad
phpdvlpmt <37871492+phpdvlpmt@users.noreply.github.com>
2024-01-02 12:44:53 +0100

    edit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 const Quiz = () => {
   return (
+    <>
     <div className="flex flex-col gap-5 text-xl font-bold">
       <Link href="/q1">Naše vlast</Link>
       <Link href="/q2">Mapy</Link>
@@ -9,7 +10,7 @@ const Quiz = () => {
       <Link href="/q4">Zemědělství</Link>
       <Link href="/q5">Nerostné suroviny, průmysl</Link>
       <Link href="/q6">Evropa - jeden ze světadílů</Link>
-    </div>
+    </div></>
   );
 };
 
