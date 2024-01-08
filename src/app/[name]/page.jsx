d9fa5dcf8cc0e1706db8d906f2a84829975bf2ba
phpdvlpmt <37871492+phpdvlpmt@users.noreply.github.com>
2024-01-08 08:30:10 +0100

    edit

--- src/app/[name]/page.jsx
+++ src/app/[name]/page.jsx
@@ -1,24 +1,29 @@
-
-import Quizcmp from '@/components/Quizcmp';
-import { promises as fs } from 'fs';
+import Quizcmp from "@/components/Quizcmp";
+import { redirect } from "next/navigation";
+import { promises as fs } from "fs";
 
 //import  {quiz as q1}  from '../../../../data';
 ///import  {quiz as q2}  from '../../../../data2';
 
-const Q = async ({params}) => {
- 
-  const file = await fs.readFile(process.cwd() + '/files/' + params.name + '.json', 'utf8');
-  const data = JSON.parse(file);
-    
+const Q = async ({ params }) => {
+  try {
+    const file = await fs.readFile(
+      process.cwd() + "/files/" + params.name + ".json",
+      "utf8"
+    );
+    const data = JSON.parse(file);
+  } catch (error) {
+    console.log(error);
+    redirect("/");
+  }
+
   //const {quiz2 }= await import ("../../../../data2")
-    
+
   return (
     <>
-    <Quizcmp quiz={data}/>
-    
+      <Quizcmp quiz={data} />
     </>
+  );
+};
 
-  )
-}
-
-export default Q
+export default Q;
--- src/app/quiz/[name]/page.jsx
+++ src/app/quiz/[name]/page.jsx
@@ -1,14 +1,21 @@
 
 import Quizcmp from '@/components/Quizcmp';
+import { redirect } from 'next/navigation'
 import { promises as fs } from 'fs';
 
 //import  {quiz as q1}  from '../../../../data';
 ///import  {quiz as q2}  from '../../../../data2';
 
 const Q = async ({params}) => {
- 
-  const file = await fs.readFile(process.cwd() + '/files/' + params.name + '.json', 'utf8');
+  try {
+    const file = await fs.readFile(process.cwd() + '/files/' + params.name + '.json', 'utf8');
   const data = JSON.parse(file);
+  } catch (error) {
+    console.log(error);
+    redirect('/')
+  }
+ 
+  
     
   //const {quiz2 }= await import ("../../../../data2")
     
