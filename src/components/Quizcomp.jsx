d443971e458cb15811b621c9149d921085c48a3b
phpdvlpmt <phpdvlpmt@gmail.com>
2024-01-20 20:54:58 +0100

    edit

--- src/components/Quizcomp.jsx
+++ src/components/Quizcomp.jsx
@@ -216,7 +216,7 @@ const Quizcomp = ({ quiz }) => {
               placeholder="Jméno"
               required
             /> */}
-            <Select name="username" required className="z-40">
+            {/* <Select name="username" required className="z-40">
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="Vyber své jméno ze seznamu." />
               </SelectTrigger>
@@ -227,7 +227,19 @@ const Quizcomp = ({ quiz }) => {
                   </SelectItem>
                 ))}
               </SelectContent>
-            </Select>
+            </Select> */}
+
+            <select
+              id="usernames"
+              name="username"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              {names.map((n, index) => (
+                <option key={index} value={n.name}>
+                  {n.name}
+                </option>
+              ))}
+            </select>
             <button type="submit" className="px-3 py-3 bg-gray-900 text-white">
               Přihlásit k testu
             </button>
