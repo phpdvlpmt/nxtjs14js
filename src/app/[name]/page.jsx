aed550033dce15b6dce57df883eabbfe20116232
phpdvlpmt <phpdvlpmt@gmail.com>
2024-04-08 17:22:36 +0200

    edit

--- src/app/[name]/page.jsx
+++ src/app/[name]/page.jsx
@@ -1,10 +1,11 @@
 import Qc from "@/components/Qc";
 import { promises as fs } from "fs";
 import { redirect } from "next/navigation";
+import path from "path";
 
 const Q = async ({ params }, data) => {
   try {
-    const file = await fs.readFile(
+    const file = await fs.readFileSync(
       process.cwd() + "/files/" + params.name + ".json",
       "utf8",
     );
