2e048002336075d6ec5034da1929084f2d6dd64f
phpdvlpmt <phpdvlpmt@gmail.com>
2024-04-08 18:13:40 +0200

    edit

--- src/app/[name]/page.jsx
+++ src/app/[name]/page.jsx
@@ -6,18 +6,18 @@ import path from "path";
 const Q = async ({ params }) => {
   let data;
   try {
-    /* const file = await fs.readFile(
+    const file = await fs.readFile(
       process.cwd() + "/files/" + params.name + ".json",
       "utf8",
-    ); */
+    );
     /* const file = await fs.readFileSync(
       path.resolve("/files/" + params.name + ".json"),
       "utf-8",
     ); */
     //data = JSON.parse(file);
-    const file = await fs.readFile(process.cwd() + "/app/q1.json", "utf8");
+    /*  const file = await fs.readFile(process.cwd() + "/app/q1.json", "utf8");
     const data = JSON.parse(file);
-    console.log(process.cwd());
+    console.log(process.cwd()); */
   } catch (error) {
     //redirect("/");
     console.log(data);
