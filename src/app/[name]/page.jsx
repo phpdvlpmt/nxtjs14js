90ba16e6e02e87695a31ad7fc85771d3ae0f7424
phpdvlpmt <phpdvlpmt@gmail.com>
2024-04-08 18:31:29 +0200

    edit

--- src/app/[name]/page.jsx
+++ src/app/[name]/page.jsx
@@ -7,8 +7,7 @@ const Q = async ({ params }) => {
   let data;
   try {
     const file = await fs.readFile(
-      process.cwd() + "/src/app/" + params.name + ".json",
-      "utf8",
+      path.resolve(process.cwd() + "/src/app/" + params.name + ".json", "utf8"),
     );
     /* const file = await fs.readFileSync(
       path.resolve("/files/" + params.name + ".json"),
