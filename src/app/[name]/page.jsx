c4376a783a2d78b4c0626376d06d115e86ca0a69
phpdvlpmt <phpdvlpmt@gmail.com>
2024-04-08 18:48:43 +0200

    edit

--- src/app/[name]/page.jsx
+++ src/app/[name]/page.jsx
@@ -1,33 +1,21 @@
 import Qc from "@/components/Qc";
 import { promises as fs } from "fs";
 import { redirect } from "next/navigation";
-import path from "path";
 
-const Q = async ({ params }) => {
-  let data;
+const Q = async ({ params }, data) => {
   try {
     const file = await fs.readFile(
-      path.resolve(process.cwd() + "/src/app/" + params.name + ".json", "utf8"),
+      process.cwd() + "/files/" + params.name + ".json",
+      "utf8",
     );
-    /* const file = await fs.readFileSync(
-      path.resolve("/files/" + params.name + ".json"),
-      "utf-8",
-    ); */
     data = JSON.parse(file);
-    /*  const file = await fs.readFile(process.cwd() + "/app/q1.json", "utf8");
-    const data = JSON.parse(file);
-    console.log(process.cwd()); */
   } catch (error) {
     //redirect("/");
-    console.log(data);
   }
   return (
     <>
-      {process.cwd()}
       <Qc quiz={data} />
-      {/*  {data.map((d, i) => (
-        <div key={i}>{d.title}</div>
-      ))} */}
+      {console.log(data)}
     </>
   );
 };
