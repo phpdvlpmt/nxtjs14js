f597c793710ba5d058543bbb0850490aa27849cd
phpdvlpmt <phpdvlpmt@gmail.com>
2024-09-10 17:21:07 +0200

    edit

--- src/app/game/page.jsx
+++ src/app/game/page.jsx
@@ -4,7 +4,7 @@ import GCard from "@/components/GCard";
 
 const GamePage = () => {
   const { questions } = gm;
-  console.log(questions);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
       {questions.map((quest) => (
